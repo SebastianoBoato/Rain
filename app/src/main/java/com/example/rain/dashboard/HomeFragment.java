c171a169c8615ef25caa46c8caa2deae999686a5
salmakam03 <897867@stud.unive.it>
2024-12-10 17:21:41 +0100

    added basic profile activity

--- app/src/main/java/com/example/rain/dashboard/HomeFragment.java
+++ app/src/main/java/com/example/rain/dashboard/HomeFragment.java
@@ -1,7 +1,9 @@
 package com.example.rain.dashboard;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
@@ -9,18 +11,35 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.rain.R;
+import com.example.rain.dashboard.profile.ProfileActivity;
+import com.example.rain.databinding.FragmentHomeBinding;
+import com.example.rain.databinding.FragmentWeatherBinding;
 
 public class HomeFragment extends Fragment {
 
     View view;
+    private FragmentHomeBinding binding;
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
-        view = inflater.inflate(R.layout.fragment_home, container, false);
+        binding = FragmentHomeBinding.inflate(inflater, container, false);
+        view = binding.getRoot();
 
-        // QUA IN MEZZO VA L'IMPLEMENTAZIONE
+        binding.profileImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getActivity(), ProfileActivity.class);
+                startActivity(intent);
+            }
+        });
 
         return view;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
     }
 }
---
+++ app/src/main/java/com/example/rain/dashboard/profile/ProfileActivity.java
@@ -0,0 +1,42 @@
+package com.example.rain.dashboard.profile;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.rain.R;
+import com.example.rain.databinding.ActivityMainBinding;
+import com.example.rain.databinding.ActivityProfileBinding;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.DocumentSnapshot;
+
+public class ProfileActivity extends AppCompatActivity {
+
+    FirebaseAuth auth = FirebaseAuth.getInstance();
+    FirebaseUser user = auth.getCurrentUser();
+    private ActivityProfileBinding binding;
+
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityProfileBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        EdgeToEdge.enable(this);
+        ViewCompat.setOnApplyWindowInsetsListener(binding.profile, (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+    }
+}
+
