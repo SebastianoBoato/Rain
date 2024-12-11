463927c9b0e3361d18f01f2e2f2f2aac3809c441
Vor7reX <hhakim815@gmail.com>
2024-12-11 16:41:29 +0100

    ho aggiunto dei commenti sul file DailyForecastActivity.java giusto per provare a fare un push

--- app/src/main/java/com/example/rain/dashboard/weather/DailyForecastActivity.java
+++ app/src/main/java/com/example/rain/dashboard/weather/DailyForecastActivity.java
@@ -17,29 +17,41 @@ import com.example.rain.recyclerview.WeatherAdapter;
 
 import java.util.List;
 
+/**
+ * Activity che mostra le previsioni meteo giornaliere.
+ * Consente di visualizzare una lista di previsioni orarie per il giorno corrente.
+ */
 public class DailyForecastActivity extends AppCompatActivity {
 
+    // Binding per accedere ai componenti del layout associato all'Activity.
     private ActivityDailyForecastBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        // Inflating del layout con View Binding.
         binding = ActivityDailyForecastBinding.inflate(getLayoutInflater());
-
         setContentView(binding.getRoot());
 
+        // Abilita modalità Edge-to-Edge per un'esperienza utente a schermo intero.
         EdgeToEdge.enable(this);
+
+        // Imposta i margini di padding per rispettare le barre di sistema (status bar e navigation bar).
         ViewCompat.setOnApplyWindowInsetsListener(binding.todayForecast, (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
 
-        // prendo la lista di meteo orario passata con l'Intent
+        // Recupera la lista delle previsioni meteo orarie passata tramite l'Intent.
         List<HourlyWeatherItem> hourlyWeatherItems = getIntent().getParcelableArrayListExtra("weatherList");
 
+        // Configura il RecyclerView per mostrare le previsioni orarie.
+        // Usa un layout manager lineare per visualizzare gli elementi in una lista verticale.
         binding.weatherRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        // Imposta l'adapter che fornisce i dati al RecyclerView.
         binding.weatherRecyclerView.setAdapter(new WeatherAdapter(getApplicationContext(), hourlyWeatherItems));
     }
 }
