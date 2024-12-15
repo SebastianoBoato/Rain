70a5b95cbd0c374532c12be0f0edb211733ccd17
elenapozzebon <896456@stud.unive.it>
2024-12-15 17:32:47 +0100

    Container. Modifica non funziona.

---
+++ app/src/main/java/com/example/rain/dashboard/container/Container.java
@@ -0,0 +1,90 @@
+package com.example.rain.dashboard.container;
+
+import java.io.Serializable;
+
+public class Container implements Serializable {
+    private final String id;
+    private  String name;
+    private final String shape;
+    private final Double param1;
+    private Double param2;
+    private final Double height;
+    private final Double roofArea;
+    private final Double area;
+    private final Double totalVolume;
+    private Double currentVolume;
+
+    public Container(String id, String name, String shape, Double param1, Double height, Double roofArea, Double area, Double totalVolume, Double currentVolume) {
+        this.id = id;
+        this.name = name;
+        this.shape = shape;
+        this.param1 = param1;
+        this.height = height;
+        this.roofArea = roofArea;
+        this.area = area;
+        this.totalVolume = totalVolume;
+        this.currentVolume = currentVolume;
+    }
+
+    public Container(String id, String name, String shape, Double param1, Double param2, Double height, Double roofArea, Double area, Double totalVolume, Double currentVolume) {
+        this.id = id;
+        this.name = name;
+        this.shape = shape;
+        this.param1 = param1;
+        this.param2 = param2;
+        this.height = height;
+        this.roofArea = roofArea;
+        this.area = area;
+        this.totalVolume = totalVolume;
+        this.currentVolume = currentVolume;
+    }
+
+    // Getter e setter
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public String getShape() {
+        return shape;
+    }
+
+    public Double getParam1() {
+        return param1;
+    }
+
+    public Double getParam2() {
+        return param2;
+    }
+
+    public Double getHeight() {
+        return height;
+    }
+
+    public Double getRoofArea() {
+        return roofArea;
+    }
+
+    public Double getBaseArea() {
+        return area;
+    }
+
+    public Double getTotalVolume() {
+        return totalVolume;
+    }
+
+    public Double getCurrentVolume() {
+        return currentVolume;
+    }
+
+    public void setCurrentVolume(Double currentVolume) {
+        this.currentVolume = currentVolume;
+    }
+}
